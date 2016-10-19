ecaa6c68bd27403651dcc0f3c195084bf09e580e
cybutek <admin@cybutek.net>
2016-10-19 17:16:04 +0100

    Created LookAtTarget script for attaching to camera to keep the monowheel in shot whilst debugging.

---
+++ Assets/Scripts/LookAtTarget.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+/// <summary>
+///     Rotates parent transform to look at target transform.
+/// </summary>
+public class LookAtTarget : MonoBehaviour
+{
+    [SerializeField]
+    private Transform targetTransform = null;
+
+    private void Update()
+    {
+        if (targetTransform != null)
+        {
+            transform.LookAt(targetTransform);
+        }
+    }
+}
