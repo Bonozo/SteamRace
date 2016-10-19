5ede67085871772fc5cdb7ba59721de01dbb6d27
Bonozo <bonozogames@gmail.com>
2016-10-19 18:56:25 +0100

    Moved camera into the monowheel and implemented mouse look.

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    private void OnApplicationFocus(bool focus)
+    {
+        if (focus)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (Cursor.lockState == CursorLockMode.Locked)
+        {
+            Vector3 localEulerAngles = transform.localEulerAngles;
+            localEulerAngles.y = localEulerAngles.y + Input.GetAxisRaw("Mouse X");
+            localEulerAngles.x = localEulerAngles.x - Input.GetAxisRaw("Mouse Y");
+            transform.localEulerAngles = localEulerAngles;
+        }
+    }
+}
