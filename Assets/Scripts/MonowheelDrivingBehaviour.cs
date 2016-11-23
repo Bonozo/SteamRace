2305b4c61d1a0905c6f24fa7e53b67b62fb5bf13
Bonozo <bonozogames@gmail.com>
2016-11-23 14:46:06 +0000

    Added gyroscopic pickup whilst accelerating.

--- Assets/Scripts/MonowheelDrivingBehaviour.cs
+++ Assets/Scripts/MonowheelDrivingBehaviour.cs
@@ -33,28 +33,32 @@ public class MonowheelDrivingBehaviour : MonoBehaviour
     });
 
     [SerializeField]
+    private AnimationCurve gyroscopicPickupRate = new AnimationCurve();
+
+    [SerializeField]
     private AudioSource engineAudioSource = null;
 
+    private float currentSpeed;
+    private float deltaSpeed;
     private float leanAngle;
 
     private void FixedUpdate()
     {
         // set angular velocity calculated from lean angle and speed
         rigidbody.angularVelocity = Vector3.up * -leanAngle * rigidbody.velocity.magnitude * 0.25f * Time.fixedDeltaTime;
-    }
 
-    private void Update()
-    {
-        // cache velocity magnitude as the current speed
-        float currentSpeed = 0.0f;
+        // set current and delta speed values
         if (rigidbody != null)
         {
+            float prevousSpeed = currentSpeed;
             currentSpeed = rigidbody.velocity.magnitude;
+            deltaSpeed = currentSpeed - prevousSpeed;
         }
 
         UpdateAcceleration();
-        UpdateTurning(currentSpeed);
-        UpdateEngineSound(currentSpeed);
+        UpdateGyroscopicPickup();
+        UpdateTurning();
+        UpdateEngineSound();
     }
 
     /// <summary>
@@ -70,7 +74,7 @@ public class MonowheelDrivingBehaviour : MonoBehaviour
             // wheel mesh rotation
             if (wheelMeshTransform != null)
             {
-                wheelMeshTransform.Rotate(Vector3.up, wheelCollider.rpm / 60 * 360 * Time.deltaTime, Space.Self);
+                wheelMeshTransform.Rotate(Vector3.up, wheelCollider.rpm / 60 * 360 * Time.fixedDeltaTime, Space.Self);
             }
         }
     }
@@ -78,7 +82,7 @@ public class MonowheelDrivingBehaviour : MonoBehaviour
     /// <summary>
     ///     Handles the engine audio.
     /// </summary>
-    private void UpdateEngineSound(float currentSpeed)
+    private void UpdateEngineSound()
     {
         if (engineAudioSource != null)
         {
@@ -87,16 +91,31 @@ public class MonowheelDrivingBehaviour : MonoBehaviour
     }
 
     /// <summary>
+    ///     Handles gyroscopic pickup from acceleration forces.
+    /// </summary>
+    private void UpdateGyroscopicPickup()
+    {
+        if (leanAngle > 0.0f)
+        {
+            leanAngle = Mathf.Clamp(leanAngle - gyroscopicPickupRate.Evaluate(deltaSpeed), 0.0f, float.MaxValue);
+        }
+        else if (leanAngle < 0.0f)
+        {
+            leanAngle = Mathf.Clamp(leanAngle + gyroscopicPickupRate.Evaluate(deltaSpeed), float.MinValue, 0.0f);
+        }
+    }
+
+    /// <summary>
     ///     Handles input and updating of the vehicle's turning.
     /// </summary>
-    private void UpdateTurning(float currentSpeed)
+    private void UpdateTurning()
     {
         float leanAmount = Input.GetAxis("Lean");
 
         // input handler
         if (Mathf.Abs(leanAmount) > 0.0f)
         {
-            leanAngle = leanAngle - (velocityLeanSpeed.Evaluate(currentSpeed) * leanAmount * Time.deltaTime);
+            leanAngle = leanAngle - (velocityLeanSpeed.Evaluate(currentSpeed) * leanAmount * Time.fixedDeltaTime);
         }
 
         // clamp the lean angle based on current speed
