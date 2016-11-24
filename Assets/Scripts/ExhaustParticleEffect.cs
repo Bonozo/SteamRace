8640558e6970c4932afa03a52a91b2706fd41fa7
Bonozo <bonozogames@gmail.com>
2016-11-24 14:09:11 +0000

    Implemented exhaust particle effects on the monowheel.

---
+++ Assets/Scripts/ExhaustParticleEffect.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ExhaustParticleEffect : MonoBehaviour
+{
+    [SerializeField]
+    private float startDelay = 0.0f;
+
+    private ParticleSystem[] particleSystems;
+
+    private void Awake()
+    {
+        particleSystems = GetComponentsInChildren<ParticleSystem>();
+    }
+
+    private void Start()
+    {
+        foreach (ParticleSystem exhaustEffect in particleSystems)
+        {
+            if (exhaustEffect != null)
+            {
+                exhaustEffect.startDelay = startDelay;
+                exhaustEffect.Play();
+            }
+        }
+    }
+}
